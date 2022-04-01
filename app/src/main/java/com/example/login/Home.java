b549360d012bfda8ebe5e154f0f7baaebf97f9a8
RoyerUnivalle <royer.estrada@correounivalle.edu.co>
2022-04-01 17:34:48 -0500

    se agrega fragmento programaticamente. Ver doc en:https://developer.android.com/guide/components/fragments?hl=es-419

--- app/src/main/java/com/example/login/Home.java
+++ app/src/main/java/com/example/login/Home.java
@@ -2,16 +2,23 @@ package com.example.login;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-public class Home extends AppCompatActivity {
+import com.example.login.fragments.FragmentButtonAdds;
 
+import java.time.Instant;
+
+public class Home extends AppCompatActivity {
     TextView labelDatos;
     Integer contador;
+    FragmentManager fragmentManager; // = getSupportFragmentManager();
+    FragmentTransaction fragmentTransaction; // = fragmentManager.beginTransaction();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +29,12 @@ public class Home extends AppCompatActivity {
         String passwd = recibo.getString("passwd");
         labelDatos.setText("User: "+ name + " Passwd: "+ passwd);
         contador = 0;
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        FragmentButtonAdds fragmento = new FragmentButtonAdds();
+        // FragmentButtonAdds fragmento = FragmentButtonAdds.newInstance("a","b");
+        fragmentTransaction.add(R.id.contenedorFragment, fragmento);
+        fragmentTransaction.commit();
     }
 
     public void contar(View l){
