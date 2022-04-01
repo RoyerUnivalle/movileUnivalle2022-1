34d0aa2d496d94a6cd9a4756ff96182cdbdfeaa0
RoyerUnivalle <royer.estrada@correounivalle.edu.co>
2022-04-01 17:06:20 -0500

    Se entrega para revisión Paso por parametros explicacion de Strings Persistir estado al cambio de configuracion Fragmentos vía xml

--- app/src/main/java/com/example/login/Home.java
+++ app/src/main/java/com/example/login/Home.java
@@ -1,17 +1,32 @@
 package com.example.login;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class Home extends AppCompatActivity {
 
+    TextView labelDatos;
+    Integer contador;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        labelDatos = findViewById(R.id.tvDatos);
+        Bundle recibo = getIntent().getExtras();
+        String name = recibo.getString("name");
+        String passwd = recibo.getString("passwd");
+        labelDatos.setText("User: "+ name + " Passwd: "+ passwd);
+        contador = 0;
+    }
+
+    public void contar(View l){
+        contador = contador + 1;
+        labelDatos.setText("contador: "+contador);
     }
 
     public void atras(View l){
@@ -19,5 +34,17 @@ public class Home extends AppCompatActivity {
         Intent ir = new Intent(this,MainActivity.class);
         ir.addFlags(ir.FLAG_ACTIVITY_CLEAR_TASK | ir.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(ir);
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("contador",contador);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        contador = savedInstanceState.getInt("contador");
     }
 }
--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.login;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -16,7 +18,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ed1 = findViewById(R.id.ed1);
-        ed1 = findViewById(R.id.ed2);
+        ed2 = findViewById(R.id.ed2);
         btn2 = findViewById(R.id.btn2);
         btn3 = findViewById(R.id.btn3);
         btn2.setOnClickListener(new View.OnClickListener() {
@@ -32,7 +34,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
        //Toast.makeText(this,"Hola boton en linea", Toast.LENGTH_SHORT).show();
         Intent ir = new Intent(this,Home.class);
         ir.addFlags(ir.FLAG_ACTIVITY_CLEAR_TASK | ir.FLAG_ACTIVITY_CLEAR_TOP);
-        startActivity(ir);
+        if(ed1.getText().toString().matches("") || ed2.getText().toString().matches("")){
+            //Toast.makeText(this,"Debe diligenciar todos los campos", Toast.LENGTH_LONG).show();
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Debe diligenciar todos los campos")
+                    .setPositiveButton(R.string.start, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            Toast.makeText(getApplicationContext(),"Dio aceptar", Toast.LENGTH_LONG).show();
+                        }
+                    })
+                    .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            Toast.makeText(getApplicationContext(),"Dio cancelar", Toast.LENGTH_LONG).show();
+                        }
+                    });
+            // Create the AlertDialog object and return it
+            // Create the AlertDialog
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        }else {
+            Bundle datos = new Bundle();
+            datos.putString("name",ed1.getText().toString());
+            datos.putString("passwd",ed2.getText().toString());
+            ir.putExtras(datos);
+            startActivity(ir);
+        }
     }
     @Override
     public void onClick(View view) {
