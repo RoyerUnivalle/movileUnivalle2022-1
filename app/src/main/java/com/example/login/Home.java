0f2ed738230ab0881d65a5f177951e3afa40ff74
RoyerUnivalle <royer.estrada@correounivalle.edu.co>
2022-04-06 10:39:39 -0500

    se entrega tarea asincrona

--- app/src/main/java/com/example/login/Home.java
+++ app/src/main/java/com/example/login/Home.java
@@ -6,9 +6,13 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.login.fragments.FragmentButtonAdds;
 
@@ -17,17 +21,21 @@ import java.time.Instant;
 public class Home extends AppCompatActivity {
     TextView labelDatos;
     Integer contador;
+    Button btnPintar;
+    Pintar objPintar;
     FragmentManager fragmentManager; // = getSupportFragmentManager();
     FragmentTransaction fragmentTransaction; // = fragmentManager.beginTransaction();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         labelDatos = findViewById(R.id.tvDatos);
+        btnPintar = findViewById(R.id.btnPintar);
         Bundle recibo = getIntent().getExtras();
         String name = recibo.getString("name");
         String passwd = recibo.getString("passwd");
-        labelDatos.setText("User: "+ name + " Passwd: "+ passwd);
+        labelDatos.setText("User: " + name + " Passwd: " + passwd);
         contador = 0;
         fragmentManager = getSupportFragmentManager();
         fragmentTransaction = fragmentManager.beginTransaction();
@@ -37,14 +45,14 @@ public class Home extends AppCompatActivity {
         fragmentTransaction.commit();
     }
 
-    public void contar(View l){
+    public void contar(View l) {
         contador = contador + 1;
-        labelDatos.setText("contador: "+contador);
+        labelDatos.setText("contador: " + contador);
     }
 
-    public void atras(View l){
+    public void atras(View l) {
         //Toast.makeText(this,"Hola boton en linea", Toast.LENGTH_SHORT).show();
-        Intent ir = new Intent(this,MainActivity.class);
+        Intent ir = new Intent(this, MainActivity.class);
         ir.addFlags(ir.FLAG_ACTIVITY_CLEAR_TASK | ir.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(ir);
     }
@@ -52,12 +60,80 @@ public class Home extends AppCompatActivity {
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putInt("contador",contador);
+        outState.putInt("contador", contador);
     }
 
     @Override
     protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
         contador = savedInstanceState.getInt("contador");
+    }
+
+    // demostración que la UI esta acomplada al hilo principal de la app
+    public void pintar(View p) {
+        /*for (int i = 0; i < contador; i++) {
+            btnPintar.setBackgroundColor(Color.rgb(aleatorio(),aleatorio(),aleatorio()));
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }*/
+        // Opcion 2  por medio de Runnable
+        /*new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i <= contador; i++) {
+                    //System.out.println(aleatorio());
+                    btnPintar.setBackgroundColor(Color.rgb(aleatorio(), aleatorio(), aleatorio()));
+                    btnPintar.setText("Pintar: " + i);
+                    //if(i == contador){
+                        //Toast.makeText(getApplicationContext(), "Terminé",Toast.LENGTH_LONG).show();
+                    //}
+                    try {
+                        Thread.sleep(1000); //<-- en este contexto hace referencia al hilo recien creado
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+        */
+        //Toast.makeText(getApplicationContext(), "Terminé", Toast.LENGTH_LONG).show();
+        objPintar = new Pintar();
+        objPintar.execute();
+    }
+
+    public int aleatorio() {
+        return ((int) (Math.random() * 255 + 1));
+    }
+
+    public class Pintar extends AsyncTask<Void, Integer, Void> {
+        @Override
+        protected Void doInBackground(Void... integers) {
+            for (int i = 0; i < contador; i++) {
+                //btnPintar.setBackgroundColor(Color.rgb(aleatorio(),aleatorio(),aleatorio()));
+                publishProgress(i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        } // parametro,progeso, resultado
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            btnPintar.setBackgroundColor(Color.rgb(aleatorio(),aleatorio(),aleatorio()));
+            btnPintar.setText("Pintar: " + values[0]);
+        }
+
+        @Override
+        protected void onPostExecute(Void unused) {
+            super.onPostExecute(unused);
+            Toast.makeText(getApplicationContext(), "Terminé", Toast.LENGTH_LONG).show();
+        }
     }
 }
